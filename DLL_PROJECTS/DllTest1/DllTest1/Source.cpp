e8ccafae89d419fc0c0e176ca126108e0934480c
alexpagri <alexstef34@yahoo.com>
2016-04-13 21:30:41 +0300

    Com: Vladeas First Test

--- DLL_PROJECTS/DllTest1/DllTest1/Source.cpp
+++ DLL_PROJECTS/DllTest1/DllTest1/Source.cpp
@@ -1,3 +1,5 @@
+#define DllExport extern "C++" __declspec(dllexport)
+#include <windows.h>
 #include <iostream> 
 #include <fstream>
 #include <random>
@@ -6,86 +8,92 @@
 
 using namespace std; 
 //n - numar lini ; m - numar coloane
+
+
+string appdata = getenv("LOCALAPPDATA");
+string text1, text2;
  
 
-void citire(int &n,int &m,int mat[500][500],int &tr_l,int &tr_c,int &k,int &Nr_spati_libere_a_lui_Alex)
+DllExport void citire(int &n, int &m, int mat[500][500], int &tr_l, int &tr_c, int &k, int &Nr_spati_libere_a_lui_Alex)
 {
-	ifstream f("text.txt");
+	ifstream f(appdata + text1);
 	int i, j;
-	k=0;
-	f>>n>>m;
-	f>>Nr_spati_libere_a_lui_Alex;
-	f>>tr_l>>tr_c;// pozitie obiect inamic
-	for(i=1;i<=n;i++)
-		for(j=1;j<=m;j++)
+	k = 0;
+	f >> n >> m;
+	f >> Nr_spati_libere_a_lui_Alex;
+	f >> tr_l >> tr_c;// pozitie obiect inamic
+	for (i = 1; i <= n; i++)
+		for (j = 1; j <= m; j++)
 		{
-			f>>mat[i][j];
-			if(mat[i][j]==1)
+			f >> mat[i][j];
+			if (mat[i][j] == 1)
 				k++;//numar cate spatii libere exista
 		}
 	f.close();
 }
 
 
-void stergere(int n,int m,int mat[500][500],int tr_l,int tr_c,int &k,int Nr_spati_libere_a_lui_Alex)
+DllExport void stergere(int n, int m, int mat[500][500], int tr_l, int tr_c, int &k, int Nr_spati_libere_a_lui_Alex)
 {
 	int i, j;
-	for(i=tr_l-Nr_spati_libere_a_lui_Alex;i<=tr_l+Nr_spati_libere_a_lui_Alex;i++)//dau valoarea '0' linilor de la pozitia data cu 3 lini in sus si 3 in jos
-		for(j=1;j<=m;j++)
+	for (i = tr_l - Nr_spati_libere_a_lui_Alex; i <= tr_l + Nr_spati_libere_a_lui_Alex; i++)//dau valoarea '0' linilor de la pozitia data cu 3 lini in sus si 3 in jos
+		for (j = 1; j <= m; j++)
 		{
-			if(mat[i][j]==1)
+			if (mat[i][j] == 1)
 				k--;//actualizez numarul de spatii libere
-			mat[i][j]=0;
+			mat[i][j] = 0;
 		}
-	for(i=1;i<=n;i++)//dau valoarea '0' coloanelor de la pozitia data cu 3 coloane in sus si 3 in jos
-		for(j=tr_c-Nr_spati_libere_a_lui_Alex;j<=tr_c+Nr_spati_libere_a_lui_Alex;j++)
+	for (i = 1; i <= n; i++)//dau valoarea '0' coloanelor de la pozitia data cu 3 coloane in sus si 3 in jos
+		for (j = tr_c - Nr_spati_libere_a_lui_Alex; j <= tr_c + Nr_spati_libere_a_lui_Alex; j++)
 		{
-			if(mat[i][j]==1)
+			if (mat[i][j] == 1)
 				k--;//actualizez numarul de spatii libere
-			mat[i][j]=0;
+			mat[i][j] = 0;
 		}
 }
 
 
 
-void transformare(int n,int m,int mat[500][500],int p)
+DllExport void transformare(int n, int m, int mat[500][500], int p)
 {
 	int i, j;
-	ofstream g("alex.nume");
-	for(i=1;i<=n && p!=0;i++)
-		for(j=1;j<=m && p!=0;j++)
-			if(mat[i][j]==1)//trec prin pozitile libere
+	ofstream g(appdata + text2);
+	for (i = 1; i <= n && p != 0; i++)
+		for (j = 1; j <= m && p != 0; j++)
+			if (mat[i][j] == 1)//trec prin pozitile libere
 			{
 				p--;//scad din numarul ales aleatoriu pana cand ajunge la 0
-				if(p==0)
-					g<<i<<" "<<j<<endl;//cand ajunge la 0 afisez ultima pozitie verificata
+				if (p == 0)
+					g << i << " " << j << endl;//cand ajunge la 0 afisez ultima pozitie verificata
 			}
 	g.close();
 }
 
 
-void afisare(int n,int m,int mat[500][500])
+DllExport void afisare(int n, int m, int mat[500][500])
 {
-	int i,j;
-	for(i=1;i<=n;i++)
+	int i, j;
+	for (i = 1; i <= n; i++)
 	{
-		for(j=1;j<=m;j++)
-			cout<<mat[i][j]<<" ";
-		cout<<endl;
+		for (j = 1; j <= m; j++)
+			cout << mat[i][j] << " ";
+		cout << endl;
 	}
 }
 
 
-int main()
+DllExport double spawn_vladeas_pro(char wd[], char wd_out[])
 {
-	int n=0,m=0,mat[500][500],tr_l,tr_c,p,Nr_spati_libere_a_lui_Alex;
+	text1 = wd;
+	text2 = wd_out;
+	int n = 0, m = 0, mat[500][500], tr_l, tr_c, p, Nr_spati_libere_a_lui_Alex;
 	int k;
-	citire(n,m,mat,tr_l,tr_c,k,Nr_spati_libere_a_lui_Alex);//citesc matricea si pozitia initiala
-	stergere(n,m,mat,tr_l,tr_c,k,Nr_spati_libere_a_lui_Alex);//sterg pozitile din jurul pozitiei initiale 
+	citire(n, m, mat, tr_l, tr_c, k, Nr_spati_libere_a_lui_Alex);//citesc matricea si pozitia initiala
+	stergere(n, m, mat, tr_l, tr_c, k, Nr_spati_libere_a_lui_Alex);//sterg pozitile din jurul pozitiei initiale 
 	default_random_engine Random(time(0));
-	uniform_int_distribution<int> Roll(1,k);
-	p= Roll(Random);// aleg un numar aleatoriu dintre '1' si numarul de spatii libere ramase 
-	transformare(n,m,mat,p);//folosindu-ma de numarul ales aleatoriu o sa aleg o pozitie libera la intamplare
+	uniform_int_distribution<int> Roll(1, k);
+	p = Roll(Random);// aleg un numar aleatoriu dintre '1' si numarul de spatii libere ramase 
+	transformare(n, m, mat, p);//folosindu-ma de numarul ales aleatoriu o sa aleg o pozitie libera la intamplare
 	//afisare(n,m,mat);
-	system("pause");
+	return 0.0;
 }
